b1cf42f47b6c1e3b8eeeb7fbf1341b703a3490e9
Bang Dao <daotranbang@gmail.com>
2026-02-09 05:58:24 +0700

    Upgrade to agp 9.0.0

--- android/app/src/main/kotlin/com/vinfast/vf3smart/auto/VF3AutoService.kt
+++ android/app/src/main/kotlin/com/vinfast/vf3smart/auto/VF3AutoService.kt
@@ -3,7 +3,6 @@ package com.vinfast.vf3smart.auto
 import androidx.car.app.CarAppService
 import androidx.car.app.Session
 import androidx.car.app.validation.HostValidator
-import com.vinfast.vf3smart.BuildConfig
 import com.vinfast.vf3smart.data.repository.VF3Repository
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -15,13 +14,8 @@ class VF3AutoService : CarAppService() {
     lateinit var repository: VF3Repository
 
     override fun createHostValidator(): HostValidator {
-        return if (BuildConfig.DEBUG) {
-            HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
-        } else {
-            HostValidator.Builder(applicationContext)
-                .addAllowedHosts(androidx.car.app.R.array.hosts_allowlist_sample)
-                .build()
-        }
+        // Use ALLOW_ALL_HOSTS_VALIDATOR for development to avoid "Package DENIED" errors
+        return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
     }
 
     override fun onCreateSession(): Session {
